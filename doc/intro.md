088a6f56365a8ce7279e26ef52dcc2528434cb7b
Yuan Xue <ziyaajiang@gmail.com>
2014-09-17 00:47:42 +0800

    that is all

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to happy-dashboard
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/happy_dashboard/core_test.clj
@@ -0,0 +1,7 @@
+(ns happy-dashboard.core-test
+  (:require [clojure.test :refer :all]
+            [happy-dashboard.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
